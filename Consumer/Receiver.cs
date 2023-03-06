a344627bf431b2cc34cf5b479d9009a454a43504
Alessandro Dias <alessandro.dias@ambevtech.com.br>
2023-03-06 10:34:04 -0400

    Atualização do projeto

--- Consumer/Receiver.cs
+++ Consumer/Receiver.cs
@@ -1,44 +1,43 @@
-﻿using RabbitMQ.Client;
-using System;
+﻿using System;
 using System.Text;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-namespace Consumidor
+namespace Consumer;
+
+public class Receiver
 {
-	public class Receiver
-	{
-		static void Main(string[] args)
-		{
-			Console.Title = "RECEIVER";
+    static void Main(string[] args)
+    {
+        Console.Title = "RECEIVER";
 
-			var factory = new ConnectionFactory() { HostName = "localhost" };
+        var factory = new ConnectionFactory { HostName = "localhost" };
 
-			using (var connection = factory.CreateConnection())
-			{
-				using (var channel = connection.CreateModel())
-				{
-					channel.QueueDeclare(queue: "WillianTest",
-						durable: false,
-						exclusive: false,
-						autoDelete: false,
-						arguments: null);
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "WillianTest",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
 
-					var consumer = new EventingBasicConsumer(channel);
-					consumer.Received += (model, ea) =>
-					{
-						var body = ea.Body;
-						var message = Encoding.UTF8.GetString(body);
-						Console.WriteLine(message);
-					};
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.Span;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(message);
+                };
 
-					channel.BasicConsume(queue: "WillianTest",
-						autoAck: true,
-						consumer: consumer);
+                channel.BasicConsume(queue: "WillianTest",
+                    autoAck: true,
+                    consumer: consumer);
 
-					Console.WriteLine("Consumidor funcionando");
-					Console.ReadLine();
-				}
-			}
-		}
-	}
+                Console.WriteLine("Consumidor funcionando");
+                Console.ReadLine();
+            }
+        }
+    }
 }
--- Publisher/Sender.cs
+++ Publisher/Sender.cs
@@ -2,38 +2,37 @@
 using System.Text;
 using RabbitMQ.Client;
 
-namespace TestRabbitMQ
+namespace Publisher;
+
+public class Sender
 {
-	public class Sender
-	{
-		static void Main(string[] args)
-		{
-			Console.Title = "SENDER";
+    static void Main(string[] args)
+    {
+        Console.Title = "SENDER";
 
-			var factory = new ConnectionFactory() { HostName = "localhost" };
+        var factory = new ConnectionFactory() { HostName = "localhost" };
 
-			using (var connection = factory.CreateConnection())
-			using (var channel = connection.CreateModel())
-			{
-				channel.QueueDeclare(queue: "WillianTest",
-					durable: false,
-					exclusive: false,
-					autoDelete: false,
-					arguments: null);
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "WillianTest",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
 
-				string message = "Tutorial RabbitMQ!!!!!";
-				var body = Encoding.UTF8.GetBytes(message);
+            string message = "Tutorial RabbitMQ!!!!!";
+            var body = Encoding.UTF8.GetBytes(message);
 
-				channel.BasicPublish(exchange: "",
-					routingKey: "WillianTest",
-					basicProperties: null,
-					body: body);
+            channel.BasicPublish(exchange: "",
+                routingKey: "WillianTest",
+                basicProperties: null,
+                body: body);
 
-				Console.WriteLine(" [x] Sent {0}", message);
-			}
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
 
-			Console.WriteLine(" Press [enter] to exit.");
-			Console.ReadLine();
-		}
-	}
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
 }
